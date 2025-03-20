93470d6168eb7410fb9df195597672ec00b8b42c
Coutinho-David <davidcoutinho555@gmail.com>
2025-03-20 14:24:00 +0000

    Updated parsing, populate and graph.h

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ int main() {
 
     Graph<std::string> graph = populate();
 
-    auto v = graph.getVertexSet();
+    
+   // auto v = graph.getVertexSet();
    // for (auto elem : v) {
-   //     std::cout << elem->getInfo() << "\n";
+   //     std::cout << elem->getInfo() << " " << elem->parking << "\n";
    //}
 
 }
