4f09cddba0a15ca7bc591fdf7b2863bbcd88dbf3
andre <a.martimpinho@gmail.com>
2025-03-20 12:31:11 +0000

    Changes to structure

--- cl_menu.h
+++ cl_menu.h
@@ -9,59 +9,48 @@
 using namespace std;
 
 void cl_points(string &source, string &destination); //Source and destination selection
-void cl_mode(string &mode, string source, string destination); //Mode of transportation selection
+void cl_mode(int &mode, string source, string destination); //Mode of transportation selection
 void cl_max_walk(int &maxWalkTime);
 void cl_avoid_nodes(string &avoidNodesType, string start_point, string destination);
 void cl_avoid_routes(string &avoidSegmentsType, string start_point, string destination);
-void fileHandler(string &mode, string &source, string &destination, int &maxWalkTime, vector<int> &avoidNodesFile, vector<pair<int, int>> &avoidSegmentsFile);
+void fileHandler(int &mode, string &source, string &destination, int &maxWalkTime, vector<int> &avoidNodesFile, vector<pair<int, int>> &avoidSegmentsFile);
 
-int init() {
-    
-    int input;
+void init(int &input,
+    int &mode,
+    string &source,
+    string &destination,
+    string &avoidNodesType,
+    string &avoidSegmentsType,
+    int &maxWalkTime,
+    vector<int> &avoidNodesFile,
+    vector<pair<int, int>> &avoidSegmentsFile) {
     cout << "Welcome to the individual route planning tool!\n" << endl;
     cout << "Would you like to submit a file or type your preferences?\n1 - \"File\"\n2 - \"Type\"" << endl << "Option: ";
     cin >> input;
 
     if (!(input == 1 || input == 2)) {
         cout << "Wrong input!" << endl;
-        init();
+        init(input, mode, source, destination, avoidNodesType, avoidSegmentsType, maxWalkTime, avoidNodesFile, avoidSegmentsFile);
     }
 
-    return input;
-}
-
-int main() {
-
-     string mode = "";
-     string  source = "";
-     string destination = "";
-     string  avoidNodesType = "";
-     string avoidSegmentsType = "";
-     int maxWalkTime = 0;
-     vector<int> avoidNodesFile = {};
-     vector<pair<int, int>> avoidSegmentsFile = {};
-
-    int input = init();
-    
     if (input == 1) fileHandler(mode, source, destination, maxWalkTime, avoidNodesFile, avoidSegmentsFile);
-    
+
     else {
         cl_points(source, destination);
         cl_mode(mode, source, destination);
-        if (mode == "driving-walking") cl_max_walk(maxWalkTime);
+        if (mode == 3) cl_max_walk(maxWalkTime);
         cl_avoid_nodes(avoidNodesType, source, destination);
         cl_avoid_routes(avoidSegmentsType, source, destination);
-        }
-
-    return 0;
+    }
 }
+
 
 void cl_points(string &source, string &destination) {
     cout << "Where are you starting your trip?" << endl << "Location: ";
     cin >> source;
     cout << "Where would you like to go?" << endl << "Location: ";
     cin >>  destination;
-    
+
     transform(source.begin(), source.end(), source.begin(),[](unsigned char c){
       return toupper(c);
     });
@@ -75,46 +64,32 @@ void cl_points(string &source, string &destination) {
     }
 }
 
-void cl_mode(string &mode, string source, string destination) {
+void cl_mode(int &mode, string source, string destination) {
     cout << "How would you like to go from " << source << " to " << destination << "?" << endl;
     cout << "We recommend choosing one of the sustained modes!" << endl;
     cout << "Please pick one of the options bellow: \n 1 - Driving; \n 2 - Walking; \n 3 - Both." << endl << "Option: ";
     cin >> mode;
-    if (!(mode == "1" || mode == "2" || mode == "3")) {
+    if (!(mode == 1 || mode == 2 || mode == 3)) {
         cout << "Invalid mode!" << endl;
         cl_mode(mode, source, destination);
-    }
-    switch (stoi(mode)) {
-        case '1': mode = "driving"; break;
-        case '2': mode = "walking"; break;
-        case '3': mode = "driving-walking"; break;
     }
 }
 
 void cl_max_walk(int &maxWalkTime) {
-    string input = "";
-    cout << "What is the maximum acceptable walking time?" << endl;
-    cin >> input;
-    maxWalkTime = stoi(input);
+    cout << "What is the maximum acceptable walking time in minutes?" << endl;
+    cin >> maxWalkTime;
 }
 
 void cl_avoid_nodes(string &avoidNodesType,string source, string destination) {
-    bool control = 1;
     cin.ignore();
-    while (control) {
-        cout << "Is there any places you would like to avoid?" << endl << "Separate them with spaces." << endl << "(Press enter to finish): ";
-        getline(cin,avoidNodesType);
-        transform(avoidNodesType.begin(), avoidNodesType.end(), avoidNodesType.begin(),[](unsigned char c){
-            return toupper(c);});
-        if (avoidNodesType.find(source) != string::npos || avoidNodesType.find(destination) != string::npos) {
-            cout << "Can not avoid source nor destination." << endl;
-            avoidNodesType = "";
-        }
-        else {
-            control = 0;
-        }
+    cout << "Is there any places you would like to avoid?" << endl << "Separate them with spaces." << endl << "(Press enter to finish): ";
+    getline(cin,avoidNodesType);
+    transform(avoidNodesType.begin(), avoidNodesType.end(), avoidNodesType.begin(),[](unsigned char c){
+        return toupper(c);});
+    if (avoidNodesType.find(source) != string::npos || avoidNodesType.find(destination) != string::npos) {
+        cout << "Can not avoid source nor destination." << endl;
+        avoidNodesType = "";
     }
-    cout << avoidNodesType << endl;
 }
 
 void cl_avoid_routes(string &avoidSegmentsType, string source, string destination) {
@@ -128,11 +103,10 @@ void cl_avoid_routes(string &avoidSegmentsType, string source, string destinatio
         cout << "Can not avoid source nor destination" << endl;
         cl_avoid_routes(avoidSegmentsType, source, destination);
     }
-    cout << avoidSegmentsType << endl;
 }
 
 
-void fileHandler(string &mode, string &source, string &destination, int &maxWalkTime, vector<int> &avoidNodesFile, vector<pair<int,int>> &avoidSegmentsFile) {
+void fileHandler(int &mode, string &source, string &destination, int &maxWalkTime, vector<int> &avoidNodesFile, vector<pair<int,int>> &avoidSegmentsFile) {
     string file_name;
     string parse;
     cout << "Please submit the name of the file" << endl;
@@ -145,8 +119,11 @@ void fileHandler(string &mode, string &source, string &destination, int &maxWalk
         string split = "";
         string var = parse.substr(0, parse.find(":"));
         string value = parse.substr(parse.find(":")+1);
-        cout << var << " " << value << endl;
-        if (var == "Mode") mode = value;
+        if (var == "Mode") {
+            if (value == "driving") {mode = 1;}
+            else if (value == "walking") {mode = 2;}
+            else if (value == "driving-walking") {mode = 3;}
+        }
         if (var == "Source") source = value;
         if (var == "Destination") destination = value;
         if (var == "MaxWalkTime") maxWalkTime = stoi(value);
@@ -154,7 +131,6 @@ void fileHandler(string &mode, string &source, string &destination, int &maxWalk
             value += ',';
             stringstream ss(value);
             while (getline(ss, split, ',')) {
-                cout << split << endl;
                 avoidNodesFile.push_back(stoi(split));
             }
         }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "populateGraph.h"
+#include "cl_menu.h"
 
+using namespace std;
 
 int main() {
+    int input;
+    int mode;
+    string  source = "";
+    string destination = "";
+    string  avoidNodesType = "";
+    string avoidSegmentsType = "";
+    int maxWalkTime = 0;
+    vector<int> avoidNodesFile = {};
+    vector<pair<int, int>> avoidSegmentsFile = {};
 
+    init(input, mode, source, destination, avoidNodesType, avoidSegmentsType, maxWalkTime, avoidNodesFile, avoidSegmentsFile);
     Graph<std::string> graph = populate();
 
     auto v = graph.getVertexSet();
